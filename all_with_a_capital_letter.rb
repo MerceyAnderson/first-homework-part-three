c43863296496b06ca3d65dd4ff1cb465beafce72
MerceyAnderson <43886391+MerceyAnderson@users.noreply.github.com>
2018-10-05 23:26:41 +0300

    Create all_with_a_capital_letter.rb

---
+++ all_with_a_capital_letter.rb
@@ -0,0 +1,21 @@
+# Приложение,которое требует ввода от пользователя и приветствует трижды.
+# Модификация предыдущего пункта,но каждый раз когда мы выводим приветствие,мы выводим его : b) в верхнем регистре.
+
+print "What's your name? "
+name = gets.chomp
+name.capitalize
+
+puts # Не понял как абзацы делать(
+
+print "You're password : "
+password = gets.chomp
+
+puts # Не понял как абзацы делать( (x2)
+
+puts  "Authorization completed"
+
+puts # Не понял как абзацы делать( (x3)
+
+puts "Welcome #{name}"
+
+puts "Hello! ".upcase * 3
